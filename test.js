c3b8030aa338e2529a8cdb5bf164b46aa3015d9b
Marek <marekz@gmail.com>
2017-10-21 21:37:08 -0400

    Format

--- test.js
+++ test.js
@@ -40,46 +40,22 @@ test('extractWorkbookHeaders', t => {
 });
 
 test('extractSheetHeaders', async t => {
-    const file = await fs.readFile('sample.xlsx');
-    const workbook = xlsx.read(file);
-    const sheet = workbook.Sheets[workbook.SheetNames[0]];
-    const preview = extractSheetPreview(sheet);
-    
-    t.deepEqual(preview, [
-      [
-        1,
-        2,
-        3
-      ],
-      [
-        "Janaye",
-        "Marjorie",
-        "Shea"
-      ],
-      [
-        "Biasioni",
-        "Penhaligon",
-        "Saldler"
-      ],
-      [
-        "jbiasioni0@flavors.me",
-        "mpenhaligon1@washingtonpost.com",
-        "ssaldler2@google.com"
-      ],
-      [
-        "2017/03/26",
-        "2016/11/07",
-        "2017/02/24"
-      ],
-      [
-        "Wang Sai Phun",
-        "Pul-e Sangī",
-        "Doctor Juan León Mallorquín"
-      ],
-      [
-        "Macaque, pig-tailed",
-        "Hyena, striped",
-        "Long-tailed skua"
-      ]
-    ])
+  const file = await fs.readFile('sample.xlsx');
+  const workbook = xlsx.read(file);
+  const sheet = workbook.Sheets[workbook.SheetNames[0]];
+  const preview = extractSheetPreview(sheet);
+
+  t.deepEqual(preview, [
+    [1, 2, 3],
+    ['Janaye', 'Marjorie', 'Shea'],
+    ['Biasioni', 'Penhaligon', 'Saldler'],
+    [
+      'jbiasioni0@flavors.me',
+      'mpenhaligon1@washingtonpost.com',
+      'ssaldler2@google.com'
+    ],
+    ['2017/03/26', '2016/11/07', '2017/02/24'],
+    ['Wang Sai Phun', 'Pul-e Sangī', 'Doctor Juan León Mallorquín'],
+    ['Macaque, pig-tailed', 'Hyena, striped', 'Long-tailed skua']
+  ]);
 });
